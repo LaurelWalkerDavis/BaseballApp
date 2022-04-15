479e20c9bc7bc10d0e5dc0262472629ae5e89481
Laurel <llw0008@auburn.edu>
2022-04-15 15:44:59 -0500

    Added base template Added team template

--- League_App/urls.py
+++ League_App/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'League_App'
 urlpatterns = [
+    # Home page.
     path('', views.index, name='index'),
+    # Teams page.
+    path('team/', views.teams, name='team')
 ]
--- League_App/views.py
+++ League_App/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from .models import Team
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'League_App/index.html')
+
+
+def teams(request):
+    """Show all teams"""
+    all_teams = Team.objects.order_by('team_name')
+    context = {'team': all_teams}
+    return render(request, 'League_App/team.html', context)
+
