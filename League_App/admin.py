02dc53531c26a31d133abfa626cedbbeaea21b86
Laurel <llw0008@auburn.edu>
2022-04-15 13:12:20 -0500

    Seth tried to fix repository

---
+++ League_App/admin.py
@@ -0,0 +1,7 @@
+from .models import Player, Team
+from django.contrib import admin
+
+# Register your models here.
+admin.site.register(Player)
+admin.site.register(Team)
+
---
+++ League_App/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+
+
+# Create your models here.
+class Player(models.Model):
+    team = models.ForeignKey('Team', related_name='players', related_query_name='player',
+                             default=None, on_delete=models.DO_NOTHING)
+    first_name = models.CharField(max_length=200, default=None)
+    last_name = models.CharField(max_length=200, default=None)
+    hits = models.IntegerField(default=0)
+    doubles = models.IntegerField(default=0)
+    triples = models.IntegerField(default=0)
+    at_bats = models.IntegerField(default=0)
+    home_runs = models.IntegerField(default=0)
+    rbi = models.IntegerField(default=0)
+    batting_average = models.DecimalField(max_digits=4, decimal_places=3, default=.000)
+    putouts = models.IntegerField(default=0)
+    assists = models.IntegerField(default=0)
+    errors = models.IntegerField(default=0)
+    chances = models.IntegerField(default=0)
+    fielding_percentage = models.DecimalField(max_digits=4, decimal_places=3, default=0.000)
+
+    def __str__(self):
+        return f"Player <{self.first_name} {self.last_name}, \nHits: {self.hits}, \nAt Bats: {self.at_bats}, " \
+               f"\nRBIs: {self.rbi}, \nHome Runs: {self.home_runs} \nBatting Avg: {self.batting_average}" \
+               f"\n Putouts: {self.putouts}, \nAssists: {self.assists}, \nErrors: {self.errors}," \
+               f"\n Chances: {self.chances}, \nFielding Percentage: {self.fielding_percentage}>"
+
+    class Meta:
+        indexes = [models.Index(fields=['last_name'])]
+        ordering = ['-last_name']
+
+
+class Team(models.Model):
+    team_name = models.CharField(max_length=200, default=None)
+    t_players = models.ManyToManyField(Player, related_name='players', related_query_name='player')
+    t_hits = models.IntegerField(default=0)
+    t_doubles = models.IntegerField(default=0)
+    t_triples = models.IntegerField(default=0)
+    t_home_runs = models.IntegerField(default=0)
+    t_at_bats = models.IntegerField(default=0)
+    t_runs = models.IntegerField(default=0)
+    t_batting_average = models.DecimalField(max_digits=4, decimal_places=3, default=.000)
+    wins = models.IntegerField(default=0)
+    losses = models.IntegerField(default=0)
+    games_played = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Team <{self.team_name}, \nHits: {self.t_hits}, \nDoubles: {self.t_doubles}, " \
+               f"\nTriples: {self.t_triples} \nHome Runs: {self.t_home_runs}, \nAt Bats: {self.t_at_bats}," \
+               f"\nBatting Avg: {self.t_batting_average}, \nWins: {self.wins}, \nLosses: {self.losses}, " \
+               f"\nGames Played: {self.games_played}>"
+
+    class Meta:
+        indexes = [models.Index(fields=['team_name'])]
+        ordering = ['-team_name']
+
+
