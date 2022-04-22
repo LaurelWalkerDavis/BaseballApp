d890eea1b64d61e0359d66d5b53f2cf3aae27961
Laurel <llw0008@auburn.edu>
2022-04-22 15:13:35 -0500

    User login.html and logged_out.html created Users able to sign in and out

--- M6_Project/settings.py
+++ M6_Project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'League_App',
+    'users',
 
     # default django apps
     'django.contrib.admin',
--- M6_Project/urls.py
+++ M6_Project/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.contrib.auth.models import User
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
     path('', include('League_App.urls'))
 ]
 
